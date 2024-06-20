5c038df2855d5bbf1f498a5a684609af9df870b8
sayv-xo <98850746+sayv-xo@users.noreply.github.com>
2024-06-20 19:34:11 +0000

    create book class

---
+++ week_1/LibraryManagementSystem/src/main/java/com/example/librarymanagementsystem/models/Book.java
@@ -0,0 +1,40 @@
+package com.example.librarymanagementsystem.models;
+
+public class Book {
+    private int bookID;
+    private String title;
+    private String author;
+    private String genre;
+
+    public Book(int bookID, String title, String author, String genre) {
+        this.bookID = bookID;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+    
+    public int getBookID() {
+        return bookID;
+    }
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}

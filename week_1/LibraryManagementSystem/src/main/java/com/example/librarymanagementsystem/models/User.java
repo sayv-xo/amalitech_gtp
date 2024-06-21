e46697e15c1e56680578d32c383297aa0264f9fb
sayv-xo <98850746+sayv-xo@users.noreply.github.com>
2024-06-21 02:32:53 +0000

    update book class for sql datatypes

--- week_1/LibraryManagementSystem/src/main/java/com/example/librarymanagementsystem/models/User.java
+++ week_1/LibraryManagementSystem/src/main/java/com/example/librarymanagementsystem/models/User.java
@@ -1,33 +1,52 @@
 package com.example.librarymanagementsystem.models;
 
+import javafx.beans.property.*;
+
 public class User {
-    private int userID;
-    private String name;
-    private String email;
+    private final IntegerProperty userID;
+    private final StringProperty name;
+    private final StringProperty email;
 
     public User(int userID, String name, String email) {
-        this.userID = userID;
-        this.name = name;
-        this.email = email;
+        this.userID = new SimpleIntegerProperty(userID);
+        this.name = new SimpleStringProperty(name);
+        this.email = new SimpleStringProperty(email);
     }
-    
+
     public int getUserID() {
+        return userID.get();
+    }
+
+    public IntegerProperty idProperty() {
         return userID;
     }
-    public void setUserID(int userID) {
-        this.userID = userID;
+
+    public void setUserId(int id) {
+        this.userID.set(id);
     }
+
     public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
         return name;
     }
+
     public void setName(String name) {
-        this.name = name;
+        this.name.set(name);
     }
+
     public String getEmail() {
+        return email.get();
+    }
+
+    public StringProperty emailProperty() {
         return email;
     }
+
     public void setEmail(String email) {
-        this.email = email;
+        this.email.set(email);
     }
-    
+
 }
